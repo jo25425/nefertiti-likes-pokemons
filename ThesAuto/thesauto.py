ec19cd5f96a4a7927103af073f3914166b4113d8
joanne254 <joanne254@gmail.com>
2012-12-09 20:23:02 +0000

    Threading on the way...

--- ThesAuto/thesauto.py
+++ ThesAuto/thesauto.py
@@ -44,10 +44,34 @@ import argparse
 import time
 import string
 import stat
+import multiprocessing
+import itertools
 import os
 from os.path import *
-from nltk.corpus import wordnet as wn, wordnet_ic as wn_ic
+from math import ceil
+from nltk.corpus import wordnet, brown
 from operator import itemgetter
+
+	
+## Replaces an 's' (for ADJ SAT) PoS tag by a simple 'a' (for ADJ) in order to add some smoothing
+## to the processing of PoS tags in the IC (Information Content) of a corpus
+## @return same synset, possibly with a smoothed PoS tag
+def smoothed(synset):
+	if synset.pos == 's':
+		synset.pos = 'a'
+	return synset
+		
+		
+## Lin's WordNet similarity function
+## @return similarity score, or None if there is no valid score
+def linWordnetSimilarity(word1, word2, ic):
+	
+	possibleValues = [wordnet.lin_similarity(synset1, synset2, ic) \
+				for synset1 in [smoothed(s) for s in wordnet.synsets(word1)] \
+				for synset2 in [smoothed(s) for s in wordnet.synsets(word2)] \
+				if synset1.pos == synset2.pos and synset1.pos in ic]
+	possibleValues = [v for v in possibleValues if v >= 0]
+	return max(possibleValues) if possibleValues else None
 
 
 class ThesAuto:
@@ -62,56 +86,30 @@ class ThesAuto:
 	"""
 	
 	## Initialises the parameters of the module
-	def __init__(self, inputFile, outputFile, k, discard, verbose):
+	def __init__(self, inputFile, outputFile, maxRank, discard, verbose, nbCPU):
 		## input file for words
 		self.inputFile = inputFile 
 		## output file for thesaurus
 		self.outputFile = outputFile
 		## fixed maximum rank distance
-		self.k = k
+		self.maxRank = maxRank
 		## discard option for words
 		self.discard = discard
 		## verbose option
 		self.verbose = verbose
+		## number of processors
+		self.nbCPU = nbCPU if nbCPU else multiprocessing.cpu_count()
 		## WordNet words database
 		self.database = join(dirname(realpath(__file__)), ".wordnet.words")
+		## WordNet thesaurus
+		self.thesaurus = []
 	
-	
-	## Runs the automatic construction of a thesaurus
-	def run(self):
-		## start operations
-		stime = time.time()
-		print "***************************************************************************"
-		print "WORDNET THESAURUS CREATION TOOL"
-		print "***************************************************************************\n"
-		
-		## prepare WordNet IC
-		from nltk.corpus import brown
-		brown_ic = wn.ic(brown, False, 1.0)
-		
-		## read file
-		print "[1] Extracting words from input file\n" + \
-		      "------------------------------------"
-		terms = self.extract_terms(self.inputFile, self.database, self.discard, self.verbose)
-		if self.verbose:
-			self.print_lines(terms, max=20, title="-- Extracted " + str(len(terms)) + " terms --")
-		
-		## create thesaurus
-		print "\n[2] Creating thesaurus in output file\n" + \
-		      "-------------------------------------"
-		with open(self.outputFile, 'w') as output:
-			for t in terms:
-				newSet = self.create_wordnet_neighbour_set(terms, t, brown_ic, self.k, self.verbose)
-				output.write( self.set_to_string(newSet) )
-		
-		etime = time.time()
-		print "\n>Execution took", etime-stime, "seconds"   
-	
+
 	
 	## Extracts from the specified files the term that are to appear in the thesaurus.
 	## Each term is the first field of a line.
 	## @return list of extracted terms
-	def extract_terms(self, file, database="./.wordnet.words", discard=True, verbose=False):
+	def extractTerms(self, file, database="./.wordnet.words", discard=True, verbose=False):
 		terms = []
 		tryDb, makeDb = file is None, False
 		
@@ -122,7 +120,7 @@ class ThesAuto:
 					## get 1st field
 					t = string.split(line)[0]
 					## keep if in WordNet
-					if wn.synsets(t):
+					if wordnet.synsets(t):
 						terms.append(t)
 					elif verbose:
 						print t, "not found in WordNet"
@@ -138,58 +136,111 @@ class ThesAuto:
 		
 		## find terms in WordNet
 		if makeDb:
-			for synset in list(wn.all_synsets()):
+			for synset in list(wordnet.all_synsets()):
 				terms += [lemma.name for lemma in synset.lemmas \
 					if lemma.name not in terms]
 			## write them in a file
 			if not discard:
 				open(database, 'w').write('\n'.join(terms))
 				os.chmod(database, stat.SF_IMMUTABLE)
-		return terms
-
+		return terms		
 	
-	## Replaces an 's' (for ADJ SAT) PoS tag by a simple 'a' (for ADJ) in order to add some smoothing
-	## to the processing of PoS tags in the IC (Information Content) of a corpus
-	## @return same synset, possibly with a smoothed PoS tag
-	def smoothed(self, synset):
-		if synset.pos == 's':
-			synset.pos = 'a'
-		return synset
-
-
-	## Lin's WordNet similarity function
-	def lin_wordnet_sim(self, word1, word2, ic):
+	
+	##
+	##
+	def createWordnetFullThesaurus(self, words):
+		## prepare WordNet IC and other tools
+		brownIC = wordnet.ic(brown, False, 1.0)
+		processes, fullThesaurus = [], []
+		chunkSize = int( ceil( 1.0*len(words)/self.nbCPU ) )
+		resultQueue = multiprocessing.Queue()
 		
-		possibleValues = [wn.lin_similarity(synset1, synset2, ic) \
-					for synset1 in [self.smoothed(s) for s in wn.synsets(word1)] \
-					for synset2 in [self.smoothed(s) for s in wn.synsets(word2)] \
-					if synset1.pos == synset2.pos and synset1.pos in ic]
-		return max(possibleValues) if possibleValues else -1
+		## compute thesaurus chunks in different processes
+		for counter in range(self.nbCPU):
+			chunkWords = words[counter*chunkSize:min( (counter+1)*chunkSize, len(words) )]
+			p = multiprocessing.Process(target=self.createWordnetPartialThesaurus, 
+				args=(resultQueue, chunkWords, words, brownIC, counter))
+			processes.append(p)
+			p.start()
+		
+		return sorted(itertools.chain.from_iterable(resultQueue.get() for p in processes))
+		
+	
+	
+	##
+	##
+	def createWordnetPartialThesaurus(self, queue, selectedWords, allWords, ic, nb):
+		print "-> starting  for part", nb, "with", len(selectedWords), "words"
+		partialThesaurus =  [set for set in [self.createWordnetNeighbourSet( \
+					allWords, targetWord, ic, self.maxRank) \
+					for targetWord in selectedWords] if set]
+		queue.put(partialThesaurus)
+		
+		print "<- ending process", nb
 
 
-	## Creates a neighbour set for a target word using a list of words available and an ic (Information Content
-	## of a corpus). This set can be limited to a chosen number of neighbours k.
+	## Creates a neighbour set for a target word using a list of words available and an ic 
+	## (Information Content of a corpus). This set can be limited to a chosen number of 
+	## neighbours k.
 	## Synsets whose PoS doesn't appear in the ic corpus are ignored.
-	## @return neighbour set [type: list of string and (string, float) tuples]
-	def create_wordnet_neighbour_set(self, words, targetWord, ic, k=None, verbose=False):
+	## @return neighbour set (type: [string, (string, float), (string, float), ...]
+	def createWordnetNeighbourSet(self, words, targetWord, ic, k=None, verbose=False):
 		## neighbour set creation
 		if verbose:
 			print "Creating set for " + targetWord
 
-		set = [(w, self.lin_wordnet_sim(targetWord, w, ic)) for w in words if w != targetWord]
+		set = [(w, linWordnetSimilarity(targetWord, w, ic)) 
+			for w in words if w != targetWord]
 		set.sort(key=itemgetter(1), reverse=True)
-		if k:
-			set = set[:k]
 		
-		return [targetWord] + set
+		return ([targetWord] + set[:k]) if set else None
 
 
 	## Converts a neighbour set (containing tuples) to a printable string
 	## @return string for the neighbour set
-	def set_to_string(self, set):
-		neighbour_to_string = lambda tuple: tuple[0] + '\t' + str(tuple[1])
-		return '\t'.join([set[0]] + [neighbour_to_string(w) for w in set[1:]]) + '\n'
+	def setToString(self, set):
+		neighbourToString = lambda tuple: tuple[0] + '\t' + str(tuple[1])
+		return '\t'.join([set[0]] + [neighbourToString(w) for w in set[1:]]) + '\n'
 
+	
+	## Runs the automatic construction of a thesaurus
+	def run(self):
+		## start operations
+		stime = time.time()
+		print "***************************************************************************"
+		print "WORDNET THESAURUS CREATION TOOL"
+		print "***************************************************************************\n"
+		
+		
+		
+		## read file
+		print "[1] Extracting words from input file\n" + \
+		      "------------------------------------"
+		terms = self.extractTerms(self.inputFile, self.database, self.discard, self.verbose)
+		#~ if self.verbose:
+			#~ self.print_lines(terms, max=20, title="-- Extracted " + str(len(terms)) + " terms --")
+		self.print_lines(terms, max=20, 
+			title="-- Extracted " + str(len(terms)) + " terms --")
+		
+		
+		
+		## create thesaurus
+		print "\n[2] Creating thesaurus in output file\n" + \
+		      "-------------------------------------"
+		      
+		
+		thesaurus = self.createWordnetFullThesaurus(terms)
+		self.print_lines(thesaurus, max=20, line_max=75, \
+			title="\n\n-- Constructed " + str(len(thesaurus)) + " neighbour sets --")
+			
+		with open(self.outputFile, 'w') as output:
+			for set in thesaurus:
+				output.write( self.setToString(set) )
+		
+		
+		
+		etime = time.time()
+		print "\n>Execution took", etime-stime, "seconds"
 
 	## Prints an array with customizable start, end, line length and title
 	def print_lines(self, list, min=0, max=None, line_max=None, title="List"):
@@ -220,20 +271,24 @@ if __name__=='__main__':
 		action='store', default="./wordnet.thesaurus",
 		help='output file where the thesaurus will be written (default: "./wordnet.thesaurus")')
 	# fixed maximum rank distance
-	parser.add_argument('-k', '--max-rank', type=int, dest='k',
+	parser.add_argument('-k', '--max-rank', metavar='n', type=int, dest='maxRank',
 		action='store', default=100,
 		help='number of neighbours to keep (default: 100)')
 	# discard option for words
 	parser.add_argument('-d', '--discard', dest='discard',
 		action='store_true', default=False,
-		help=' discard the record of words that appear in WordNet (default: False)')
+		help='discard the record of words that appear in WordNet (default: False)')
 	# verbose option
 	parser.add_argument('-v', '--verbose', dest='verbose', 
 		action='store_true', default=False,
 		help='display information about operations (default: False)')
+	# number of processors
+	parser.add_argument('-p', '--processors', metavar='n', dest='processors',
+		action='store',
+		help='number of processors to use (default: available)')
 		
 	a = parser.parse_args()
-	thesAuto = ThesAuto(a.inputFile, a.outputFile, a.k, a.discard, a.verbose)
+	thesAuto = ThesAuto(a.inputFile, a.outputFile, a.maxRank, a.discard, a.verbose, a.processors)
 	thesAuto.run()
 	
 
